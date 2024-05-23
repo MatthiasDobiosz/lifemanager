1e1ea12682ebe0ea31eca6ecd27e7217bd9f6330
MatthiasDobiosz <matthido98@web.de>
2024-05-23 20:20:18 +0200

    cleanup example project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,12 @@
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+const defaultUrl = "http://localhost:3000";
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "LifeManager",
+  description: "The app to manage your life",
 };
 
 export default function RootLayout({
